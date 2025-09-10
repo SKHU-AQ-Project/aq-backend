7e7f7100077ef54c7f05330d4f50d3d8dc54ba7c
Jack <devjack050700@gmail.com>
2025-09-11 02:12:51 +0900

    git 저장소 생성

---
+++ src/main/java/com/example/aq/AqApplication.java
@@ -0,0 +1,13 @@
+package com.example.aq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aq/AqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
